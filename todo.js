dd0140a58c7f62870d8ffc116e2fad44824b8e18
subaru <subaru20010218@gmail.com>
2025-06-02 23:43:31 +0900

    todoリスト

--- todo.js
+++ todo.js
@@ -1,27 +1,27 @@
 const todoList = []; //空の配列を用意
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById("addBtn").addEventListener("click", addTodo);
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("addBtn").addEventListener("click", addTodo);
 }); //html内のbtn要素を取得しaddTodoに追加
 
 function addTodo() {
   const input = document.getElementById("todoInput"); //htmlにあるinputを取得
   const newTodo = input.value.trim(); //両端の空白を除く
 
-//   入力が空の場合
-    if(newTodo === "") {
-        alert("タスクを入力してください。");
-        return;
-    };
+  //   入力が空の場合
+  if (newTodo === "") {
+    alert("タスクを入力してください。");
+    return;
+  }
 
-    // 英数字が含まれている場合の処理
-    const hasAlphaNumeric = /[a-zA-Z0-9]/.test(newTodo);
+  // 英数字が含まれている場合の処理
+  const hasAlphaNumeric = /[a-zA-Z0-9]/.test(newTodo);
   if (hasAlphaNumeric) {
     alert("英字や数字を含むToDoは追加できません。");
     return;
-  };
+  }
 
-   // 重複チェック
+  // 重複チェック
   if (todoList.includes(newTodo)) {
     alert("同じタスクが既に追加されています。");
     return;
@@ -43,8 +43,20 @@ function updateList() {
   listElement.innerHTML = "";
 
   todoList.forEach((todo, index) => {
-    const li = document.createElement("li"); //リスト作成
-    li.innerHTML = `${index + 1}. ${todo} <button onclick="deleteTodo(${index})">削除</button>`;
-    listElement.appendChild(li); //作成したリストをul内に追加
+    const li = document.createElement("li");
+
+    // テキスト設定
+    li.textContent = `${index + 1}. ${todo}`;
+
+    const btn = document.createElement("button");
+    btn.textContent = "削除する";
+
+    btn.addEventListener("click", () => {
+      deleteTodo(index);
+    });
+
+    li.appendChild(btn);
+
+    listElement.appendChild(li);
   });
 }
